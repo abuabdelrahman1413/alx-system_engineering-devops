563d63407fe1fd0509d37a2087415cf272ba9333
Mohammed Said <ms22abuabdelrahman@gmail.com>
2024-05-03 12:01:02 +0200

    1

---
+++ 0x04-loops_conditions_and_parsing/2-while_best_school.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# Using a While loop to display "Best School" 10 times
+
+# Using a While loop to display "Best School" 10 times
+i=0
+while [ $i -lt 10 ]
+do
+    echo "Best School"
+    # Increment the counter
+    i=$((i + 1))
+done
