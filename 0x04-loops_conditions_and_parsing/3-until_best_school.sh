c530d2f6295ddf419842c06785623340283c317e
Mohammed Said <ms22abuabdelrahman@gmail.com>
2024-05-03 16:07:27 +0200

    updated

---
+++ 0x04-loops_conditions_and_parsing/3-until_best_school.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# Using a until loop to display "Best School" 10 times
+
+# Using a until loop to display "Best School" 10 times
+i=0
+until [ $i -gt 9 ]
+do
+    echo "Best School"
+    # Increment the counter
+    i=$((i + 1))
+done
