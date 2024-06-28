69c58fc06c0de73f5fea043a276a48a9144f1938
abuabdelrahman1413 <ms22abuabdelrahman1413>
2024-06-28 17:33:02 +0200

    1-install flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -6,12 +6,16 @@ package { ['python3', 'python3-pip']:
 }
 
 # Install Flask version 2.1.0 and compatible Werkzeug version using pip3 provider
-package { ['Flask', 'Werkzeug']:
-  ensure   => 'installed',
-  provider => 'pip3',
-  install_options => [
-    '--upgrade', 'Flask==2.1.0',
-    '--upgrade', 'Werkzeug==2.0.3',
-  ],
-  require  => Package['python3-pip'],
+package { 'Flask':
+  ensure          => '2.1.0',
+  provider        => 'pip3',
+  install_options => ['--upgrade'],
+  require         => Package['python3-pip'],
+}
+
+package { 'Werkzeug':
+  ensure          => '2.0.3',
+  provider        => 'pip3',
+  install_options => ['--upgrade'],
+  require         => Package['python3-pip'],
 }
