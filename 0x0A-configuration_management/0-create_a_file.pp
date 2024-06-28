ca2de983454ceadcfca535b0622f5a85fdd97209
abuabdelrahman1413 <ms22abuabdelrahman1413>
2024-06-28 16:05:54 +0200

    fix 0-create

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,9 @@
-# create file name school, path /tmp, File permission is 0744, File owner is www-data, File group is www-data, File contains I love Puppet
+# 0-create_a_file.pp
 
-file {'/tmp/school':
-    ensure  => 'present',
-    content => 'I love Puppet',
-    owner   => 'www-data',
-    group   => 'www-data',
-    mode    => '0774'
+file { '/tmp/school':
+  ensure  => 'file',
+  content => 'I love Puppet',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
 }
-
