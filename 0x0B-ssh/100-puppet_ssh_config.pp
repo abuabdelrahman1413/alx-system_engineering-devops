3c281be322d6fb0d067e21e18ab0f5ede6bbe7cd
abuabdelrahman1413 <ms22abuabdelrahman1413>
2024-06-29 15:18:59 +0200

    task4

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,9 +1,14 @@
 # script configuration ssh config
+# Declare identity file
+file_line { 'Declare identity file':
+  path  => '/etc/ssh/ssh_config/config',
+  line  => 'IdentityFile ~/.ssh/school',
+  match => '^IdentityFile',
+}
 
-'Host *'                 => {
-  'SendEnv'              => 'LANG LC_*',
-  'HashKnownHosts'       => 'yes',
-  'GSSAPIAuthentication' => 'yes',
-  'PasswordAuthentication' => 'no',
-  'IdentityFile'         => '~/.ssh/school',
+# Turn off password authentication
+file_line { 'Turn off passwd auth':
+  path  => '/etc/ssh/ssh_config/config',
+  line  => 'PasswordAuthentication no',
+  match => '^PasswordAuthentication',
 }
