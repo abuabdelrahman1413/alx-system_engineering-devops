229a0a61c3e9bcb60bc631b624bcca698a5e63c6
abuabdelrahman1413 <ms22abuabdelrahman1413>
2024-06-29 16:49:31 +0200

    task4

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,19 +1,26 @@
-# configure ssh server 
-# IdentityFile ~/.ssh/school
-# password no
-
 class ssh {
     file { '/etc/ssh/ssh_config':
-        ensure => 'create',
+        ensure => 'present',
     }
     file_line { 'Turn off passwd auth':
-        ensure => 'create',
+        ensure => 'present',
         path   => '/etc/ssh/ssh_config',
         line   => 'PasswordAuthentication no',
+        match  => '^#?PasswordAuthentication\s+.*',
+        notify => Exec['Restart SSH'],
     }
     file_line { 'Declare identity file':
-        ensure => 'create',
+        ensure => 'present',
         path   => '/etc/ssh/ssh_config',
         line   => 'IdentityFile ~/.ssh/school',
+        match  => '^#?IdentityFile\s+.*',
+        notify => Exec['Restart SSH'],
+    }
+
+    exec { 'Restart SSH':
+        command     => '/etc/init.d/ssh restart',
+        refreshonly => true,
     }
 }
+
+include ssh
