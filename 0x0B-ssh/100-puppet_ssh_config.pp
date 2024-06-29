a59d9040fb82429a0a04d0eedfb65c55edcba2f0
abuabdelrahman1413 <ms22abuabdelrahman1413>
2024-06-29 17:00:05 +0200

    task4

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,11 @@
 # Puppet SSH config
-file_line { '/etc/ssh/sshd_config':
+file_line { 'Turn off passwd auth':
     ensure  => 'present',
     path    => '/etc/ssh/ssh_config',
     line    => 'PermitRootLogin no',
 }
 
-file_line { '/etc/ssh/sshd_config':
+file_line { '/Declare identity file':
     ensure  => 'present',
     path    => '/etc/ssh/ssh_config',
     line    => 'IdentityFile ~/.ssh/school',
